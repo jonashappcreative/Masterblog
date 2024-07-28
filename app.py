75c999135e38481b68aa451566fffccad0055146
Jonas Happ <github@jonashapp.com>
2024-07-28 20:52:14 +0200

    Add the index.html and add a list of all blog posts

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # add code here to fetch the job posts from a file
+
+    with open("blogposts.json", "r") as fileobj:
+        blogposts = json.load(fileobj)
+
+    return render_template("index.html", data=blogposts)
+
+
+def main():
+
+    with open("blogposts.json", "r") as fileobj:
+        blogposts = json.load(fileobj)
+        print(type(blogposts))
+
+    for blogpost in blogposts:
+        print(blogpost["id"])
+        print(blogpost["author"])
+        print(blogpost["title"])
+        print(blogpost["content"])
 
 
 if __name__ == '__main__':
-    app.run()
+    """
+    Switch between main and app.run for debugging purposes
+    main is for debugging
+    app.run is for running the server
+    """
+    # main()
+    app.run(port=5001)
---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import json
+
+
+def main():
+    """
+    """
+    blog_flask_app = Flask(__name__)
+
+    blog_flask_app._run()
+
+
+if __name__ == "__main__":
+    main()
