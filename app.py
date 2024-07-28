078663fdda97f4b54a8253cfa790d610688f9176
Jonas Happ <github@jonashapp.com>
2024-07-28 22:25:54 +0200

    Adding Add Route and html from

--- app.py
+++ app.py
@@ -1,38 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
+import uuid
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-
-    # add code here to fetch the job posts from a file
-
-    with open("blogposts.json", "r") as fileobj:
-        blogposts = json.load(fileobj)
+    # Fetch the blog posts from a file
+    try:
+        with open("blogposts.json", "r") as fileobj:
+            blogposts = json.load(fileobj)
+    except FileNotFoundError:
+        blogposts = []
 
     return render_template("index.html", data=blogposts)
 
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form.get('author')
+        title = request.form.get('title')
+        content = request.form.get('content')
 
-def main():
+        try:
+            with open("blogposts.json", "r") as fileobj:
+                blogposts = json.load(fileobj)
+        except FileNotFoundError:
+            blogposts = []
 
-    with open("blogposts.json", "r") as fileobj:
-        blogposts = json.load(fileobj)
-        print(type(blogposts))
+        next_uuid = str(uuid.uuid4())
+        next_number = len(blogposts) + 1
 
-    for blogpost in blogposts:
-        print(blogpost["id"])
-        print(blogpost["author"])
-        print(blogpost["title"])
-        print(blogpost["content"])
+        # Create a new blog post and add it to the list
+        new_post = {
+            "id": next_number,
+            "uuid": next_uuid,
+            "author": author,
+            "title": title,
+            "content": content
+        }
+
+        blogposts.append(new_post)
+
+        with open("blogposts.json", "w") as fileobj:
+            json.dump(blogposts, fileobj)
+
+        # Redirect to the home page
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
-    """
-    Switch between main and app.run for debugging purposes
-    main is for debugging
-    app.run is for running the server
-    """
-    # main()
     app.run(port=5001)
