b6bcf80c41a4a02d7cdc9cf9790801446f8d9b6a
Jonas Happ <github@jonashapp.com>
2024-07-29 20:45:16 +0200

    Added the Delete Route, not yet a user feedback of which post he deleted

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import uuid
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     # Fetch the blog posts from a file
@@ -14,6 +15,7 @@ def index():
         blogposts = []
 
     return render_template("index.html", data=blogposts)
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
@@ -49,6 +51,30 @@ def add():
         return redirect(url_for('index'))
 
     return render_template('add.html')
+
+
+@app.route('/delete/<int:post_id>')
+def delete(post_id):
+    # Find the blog post with the given id and remove it from the list
+    # Redirect back to the home page
+
+    with open("blogposts.json", "r") as fileobj:
+        blogposts = json.load(fileobj)
+
+    for post in blogposts:
+        if post["id"] == post_id:
+            index_to_pop = blogposts.index(post)
+        else:
+            continue
+
+    deleted_post = blogposts.pop(index_to_pop)
+
+    print(f"Deleted Blog Post: \n{deleted_post}")
+
+    with open("blogposts.json", "w") as fileobj:
+        json.dump(blogposts, fileobj, indent=4)
+
+    return redirect("/")
 
 
 if __name__ == '__main__':
